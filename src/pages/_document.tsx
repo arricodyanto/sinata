b7d011c2dae9f7e14e6fe096991961658f0c31e6
arricodyanto <arricohandyanto@gmail.com>
2023-08-09 02:29:41 +0700

    Allow browser to catch http backend

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				<link
+					href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600&display=swap'
+					rel='stylesheet'
+				/>
+				<meta
+					http-equiv='Content-Security-Policy'
+					content='upgrade-insecure-requests'
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
